4dc9e39e33bbf8b4b65430f9f7be19adb560b75e
Busola Okeowo <busolaokemoney@gmail.com>
2023-03-29 20:24:19 +0100

    Added config file

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,15 @@ import { FyleModule } from './fyle/fyle.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { APP_GUARD } from '@nestjs/core';
 import { AccountGuard } from './account/account.guard';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ load: [configuration], isGlobal: true }),
     AccountModule,
     FyleModule,
-    MongooseModule.forRoot('mongodb://localhost/croft'),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
   ],
   controllers: [AppController],
   providers: [
